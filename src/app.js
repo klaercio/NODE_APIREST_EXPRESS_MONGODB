69818bbd1a661fadafa185f46ed2cd82f6362a6e
klaerciom <klaerciosilva@gmail.com>
2023-06-12 08:28:22 -0300

    ↗ Feat: Rota post de livros criada

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express, { json } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
     {
         id: 1,
@@ -20,5 +22,10 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro Cadastrado com sucesso!!');
+})
 
 export default app;
